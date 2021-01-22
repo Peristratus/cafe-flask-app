39f03f9b346b3fe39b6297962ace93ec85bcdbfc
Peristratus <testcorp24@gmail.com>
2021-01-22 01:51:50 +0000

    added all routes to the pages

---
+++ run.py
@@ -0,0 +1,32 @@
+import os
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/products")
+def products():
+    return render_template("products.html")
+
+
+@app.route("/chat")
+def chat():
+    return render_template("chat.html")  
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
