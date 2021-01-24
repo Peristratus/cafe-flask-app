ba5d44dee36750f7728beebd9ae89a8368830487
Peristratus <testcorp24@gmail.com>
2021-01-24 18:51:31 +0000

    message redirects to the login page

--- run.py
+++ run.py
@@ -1,8 +1,17 @@
 import os
 from flask import Flask, render_template
+from flask import Flask, redirect
 
 
 app = Flask(__name__)
+messages =[]
+
+def add_messages(username, message):
+    messages.append("{}: {}".format(username,message))
+
+def get_all_messages():
+    """"get all of the messages and separate using a br"""
+    return "<br>".join(messages)
 
 @app.route('/')
 def index():
@@ -32,6 +41,26 @@ def contact():
 @app.route('/chatapp')
 def chatapp():
     return render_template("chatapp.html")
+
+@app.route("/")
+def home():
+    """ Main Page with instruction"""
+    return "To send message use :/user/messsage"
+
+
+
+@app.route('/<username>')
+def user(username):
+    """ Display chat Message"""
+    return "<h1>Welcome,{0}</h1>{1}".format(username, get_all_messages())
+
+
+
+@app.route('/<username>/<message>')
+def send_message(username,message):
+    """ Create a new message and redirect to the chat page"""
+    add_messages(username, message)
+    return redirect("/" + username)
 
 
 if __name__ == "__main__":
